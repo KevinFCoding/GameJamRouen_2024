9db93cbc4e479c48e4651777247b26067bc7d971
Jado <jade.bouiges@edu.itescia.fr>
2024-01-20 13:34:05 +0100

    Add Target / Timer / River / GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] River _river;
+    [SerializeField] TimerManager _timer;
+    [SerializeField] int _roundSeconds;
+
+    void Start()
+    {
+        SetUpTimer();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void SwitchAttack()
+    {
+        _river.ChangeState();
+    }
+
+    public void SetUpTimer()
+    {
+        _roundSeconds = Random.RandomRange(10, 20);
+        _timer.seconds = _roundSeconds;
+
+    }
+}
